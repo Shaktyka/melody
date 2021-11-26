6699f7883d2a034a37733560b6fe67da73bb0857
Shaktyka <k-i-s-u-n-j-a@list.ru>
2021-11-26 08:07:20 +0500

    Добавляет комментарии, меняет имя переменной

--- js/index.js
+++ js/index.js
@@ -2,20 +2,23 @@ $(document).ready(function() {
     var maxFloor = 18;
     var minFloor = 2
     var currentFloor = 2;
-    var currFloorText = 2;
+    var currFloorView = 2;
     var counterUp = $(".counter-up");
     var counterDown = $(".counter-down");
+
     $(".home-img path").on("mouseover", function() {
         currentFloor = $(this).attr('data-floor');
-        currFloorText = currentFloor < 10 ? `0${currentFloor}` : currentFloor;
-        $(".counter").text(currFloorText);
+        currFloorView = currentFloor < 10 ? `0${currentFloor}` : currentFloor;
+        $(".counter").text(currFloorView);
     });
 
+    // Обработка клика по кнопке "Вверх"
+    // Автор использует ф-цию toLocaleString("en-US", {minimumIntegerDigits: 2, useGroupping: false})
     counterUp.on("click", function() {
         if (currentFloor < maxFloor) {
             currentFloor += 1;
-            currFloorText = currentFloor < 10 ? `0${currentFloor}` : currentFloor;
-            $(".counter").text(currFloorText);
+            currFloorView = currentFloor < 10 ? `0${currentFloor}` : currentFloor;
+            $(".counter").text(currFloorView);
             $(".home-img path").removeClass('current-floor');
             $(`[data-floor=${currentFloor}]`).toggleClass('current-floor');
         }
