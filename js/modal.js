791b1b69616cf4cfc1d2e7a3ec526bd57ff1c146
Shaktyka <k-i-s-u-n-j-a@list.ru>
2021-11-27 10:43:03 +0500

    Добавляет SVG схему квартир для модального окна и анимацию при открытии окна

--- js/modal.js
+++ js/modal.js
@@ -2,12 +2,13 @@ $(document).ready(function() {
   var floorPath = $(".home-img path");
   var modal = $(".modal");
   var closeBtn = $(".btn-close");
+  var viewFlatsBtn = $(".view-flats");
 
   function toggleModal() {
     modal.toggleClass("is-open");
   }
 
   floorPath.on("click", toggleModal);
-
   closeBtn.on("click", toggleModal);
+  viewFlatsBtn.on("click", toggleModal);
 });
